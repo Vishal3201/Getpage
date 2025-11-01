384bd0d57d72615a75743f2d3692f126e662c8ce
Vishal Saroj <vishalsaroj3201@gmail.com>
2025-11-01 10:46:10 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  googleId: {
+    type: String,
+    default: null,
+  }
 });
 
 // ✅ FIX: Prevent OverwriteModelError
